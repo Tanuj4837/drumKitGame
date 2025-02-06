a7165c4feddd047d9ef306a673efe7db4b2266f2
Tanuj4837 <114664620+Tanuj4837@users.noreply.github.com>
2025-02-06 17:53:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ for(var i = 0; i<numberOfDrumButtons; i++){
         var buttonInnerHtml = this.innerHTML;
         makeSound(buttonInnerHtml);
 
-        
+        buttonAnimation(buttonInnerHtml);
 
     });
 }
 
 document.addEventListener("keypress", function(event){
     makeSound(event.key);
+    buttonAnimation(event.key); 
 });
 
 function makeSound(key){
@@ -49,9 +50,19 @@ function makeSound(key){
             break;
         
     
-        default: console.log(key)
-            break;
+        default: console.log(buttonInnerHtml)
+           
     }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout (function(){
+        activeButton.classList.remove("pressed");
+
+    }, 100);
 }
 
 
